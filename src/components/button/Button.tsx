b35c1766ee141935aad3ea5351834451a7550145
dllsc <92045376+dllsc@users.noreply.github.com>
2022-07-31 22:11:01 +0300

    base template for Button

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,17 @@
+import "./button.scss"
+import { HTMLProps, ReactNode } from 'react';
+
+interface ButtonProps extends HTMLProps<HTMLButtonElement>{
+    type?: 'delete' | 'secondary' | 'primary',
+    children: ReactNode,
+}
+const buttonTypeClassName = {
+    delete: 'button-delete',
+    secondary: 'button-secondary',
+    primary: 'button-primary'
+}
+export const Button = ({onClick, children, type = 'primary'}:ButtonProps) => {
+    const className = ['button'];
+    className.push(buttonTypeClassName[type]);
+    return <button className={className.join(' ')} onClick={onClick}>{children}</button>
+}
--- src/views/allCoinsPage.tsx
+++ src/views/allCoinsPage.tsx
@@ -1,5 +1,10 @@
+import { Button } from '../components/button/Button';
+
 export const AllCoinsPage = ()=>{
     return <>
-        All Coins Info
+        <h3>All Coins Info</h3>
+        <Button type={'delete'}>delete</Button>
+        <Button type={'primary'}>primary</Button>
+        <Button type={'secondary'}>secondary</Button>
     </>
 }
